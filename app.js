d01986cd49f9636a7b2056ba40e7cec0907f92bc
chandan <csc3641701>
2024-05-13 15:40:19 +0530

    add controller function

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const bodyParser = require("body-parser");
 // app.use(bodyParser.urlencoded({
 //     extended: true
 // }));
-app.use(bodyParser.json());
-
+// app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }))
 // const { Connection } = require('./db')
 const routes = require('./routes/index')
 
--- models/responsedataModel.js
+++ models/responsedataModel.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const responsedataModelSchema = new Schema({
-    date:{
-        type: Date,
-        required: true, 
+const responsedataModelSchema = new Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
     },
-    datestr:{
-        type: String,
-        required: true,
+    datestr: {
+      type: String,
+      required: true,
     },
-    responsedata:{
-        type: Array,
-        required: true,
+    responsedata: {
+      type: Array,
+      required: true,
     },
-    APIpassed:{
-        type: Number,
-        required: true,
+    APIpassed: {
+      type: Number,
+      required: true,
     },
-    APIfailed:{
-        type: Number,
-        required: true,
+    APIfailed: {
+      type: Number,
+      required: true,
     },
-    numberOfAPIs:{
-        type:Number,
-        required:true
-    }
-},{ strict: false})
+    numberOfAPIs: {
+      type: Number,
+      required: true,
+    },
+  },
+  { strict: false }
+);
 
-module.exports = mongoose.model('responsedata',responsedataModelSchema)
+module.exports = mongoose.model("responsedata", responsedataModelSchema);
